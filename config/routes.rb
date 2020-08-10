f2f0d1000d34971f85b425daa31065b97f8fcbd5
Tom Renard <tom.renard@zoho.com>
2020-08-10 15:46:01 +0200

    mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'answer', to: 'coaching#answer'
+  get 'ask', to: 'coaching#ask'
 end
